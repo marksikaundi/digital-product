2f66b7bc15faec41174104a7b11590258d64cac8
marksikaundi <emmanuelmark117@gmail.com>
2024-07-01 01:31:28 +0200

    chore: Update Navbar component in layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,15 @@
-export function Navbar() {
-  return <nav className="relative max-w-7xl w-full flex md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
-    <h1>Hello</h1>
+import Link from "next/link";
 
-  </nav>;
+export function Navbar() {
+  return (
+    <nav className="relative max-w-7xl w-full flex md:grid-cols-12 items-center px-4 md:px-8 mx-auto py-7">
+      <div className="md:cols-span-3">
+        <Link href="/">
+          <h1 className="text-2xl font-semibold">
+            Lupleg<span className="text-violet-500">UI</span>
+          </h1>
+        </Link>
+      </div>
+    </nav>
+  );
 }
