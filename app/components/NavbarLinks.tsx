34b46d82af8de63b90483e7f6cab6598fb23ea13
marksikaundi <emmanuelmark117@gmail.com>
2024-07-01 20:27:30 +0200

    refactor: Update NavbarLinks component in Navbar

--- app/components/NavbarLinks.tsx
+++ app/components/NavbarLinks.tsx
@@ -1,3 +1,27 @@
+export const navbarLnks = [
+  {
+    id: 0,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 1,
+    name: "Templates",
+    href: "#",
+  },
+  {
+    id: 2,
+    name: "Ui Kits",
+    href: "#",
+  },
+  {
+    id: 3,
+    name: "Icons",
+    href: "#",
+  },
+
+];
+
 export function NavbarLinks() {
   return (
     <div className="">
