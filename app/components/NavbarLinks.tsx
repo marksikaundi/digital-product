ba2d98e6132383d9fb46f2946880d1753309a3bd
marksikaundi <emmanuelmark117@gmail.com>
2024-07-15 00:35:18 +0200

    feat: Add NavbarLinks component to Navbar

--- app/components/NavbarLinks.tsx
+++ app/components/NavbarLinks.tsx
@@ -1,8 +1,30 @@
+export const navbarLinks = [
+  {
+    id: 1,
+    name: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    href: "/about",
+  },
+  {
+    id: 3,
+    name: "Services",
+    href: "/services",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    href: "/contact",
+  },
+];
 
 export function NavbarLinks() {
   return (
     <div>
       <h1>Hello from navbar links</h1>
     </div>
-  )
+  );
 }
